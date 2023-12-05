362d1f3e87db82dfe7d464fd0ba2939998edc270
Samuele Stronati <samuele.stronati@studenti.unicam.it>
2023-12-05 21:13:23 +0100

    Front-end codebase reorganized into several packages. Added API clients for the REST endpoints added through iteration3-server branch. Complete forms for API client data types, associated to CRUD operations. Support for JavaScript AG-GRID added. View interface split into several sub-views associated to a specific domain element. General app dashboard composed by those subviews. Design patterns used to implement those new features: Bridge pattern to create the front-end of a API Client data, which corresponds to elements in forms.py module; Decorator pattern to extend some of those forms functionalities; Builder pattern, as natively defined in Python language, to implement API Client detail classes.

--- client/plclient/main.py
+++ client/plclient/main.py
@@ -1,5 +1,12 @@
-from plclient.views.views import UserDashboard
 from plclient.utils.settings import users_endpoint
+from views.apiclient import UserDetail
+from views.views import UserDashboard, UserView, ShopView, FidelityProgramView, CatalogueView
+
 
 if __name__ == '__main__':
-    UserDashboard(url=users_endpoint, username='admin').open_view()
+    UserDashboard(
+        UserView(UserDetail(url=users_endpoint+'admin').get()),
+        ShopView(),
+        FidelityProgramView(),
+        CatalogueView()
+    ).open_view()
--- client/plclient/views/views.py
+++ client/plclient/views/views.py
@@ -1,16 +1,11 @@
-import streamlit as st
-import pandas as pd
-import requests
-import random
-import string
+from __future__ import annotations
 from typing import Protocol, runtime_checkable
 from dataclasses import dataclass
-from abc import ABC, abstractmethod
-from streamlit_extras.dataframe_explorer import dataframe_explorer
 from streamlit_option_menu import option_menu
-from plclient.utils.hashers import hash_password
-from plclient.utils.settings import users_endpoint, shops_endpoint, split_frame
-from plclient.utils.apiclient import UserDetail, ShopDetail, ShopList
+from plclient.utils.settings import users_endpoint, shops_endpoint, fidelity_programs_endpoint, catalogue_endpoint
+from views.uiform import UserForm, UserCreateForm, UserSetPasswordForm, Table, ShopForm, FidelityProgramForm, FidelityProgramCreateForm, CatalogueForm, CatalogueCreateForm, CatalogueUpdateForm
+from views.apiclient import UserDetail, UserList, ShopDetail, ShopList, FidelityProgramDetail, FidelityProgramList, CatalogueDetail, CatalogueList
+import streamlit as st
 
 @runtime_checkable
 class View(Protocol):
@@ -27,55 +22,19 @@ class View(Protocol):
     def close_view(self):
         ...
 
-class ClientView(ABC):
-    """
-    View instance which makes use of an underlying APIClient to 
-    retrieve from and send information to a backend, where this 
-    information is prompted by the user.
-    """
-
-    url : str
-    username : str
-    shopname : str = None
-
-    @abstractmethod
-    def open_view(self):
-        pass
-
-    @abstractmethod
-    def close_view(self):
-        pass
-
-    def get_shop(self, shopname: str):
-        return ShopDetail(api_endpoint=shops_endpoint, url=shops_endpoint+shopname).get()
-    
-    def get_user(self, username: str):
-        return UserDetail(api_endpoint=users_endpoint, url=users_endpoint+username).get()
-    
-    def get_self_user(self):
-        return self.get_user(self.username)
-    
-    def get_session_shop(self):
-        if self.shopname:
-            return self.get_shop(self.shopname)
-    
-    def get_shop_list(self):
-        return ShopList(api_endpoint=shops_endpoint).as_table(table_cols=['url','name', 'location'], column_config={'url':None})
-    
-    def user_create_form(self):
-        return UserDetail(api_endpoint=users_endpoint).as_form()
-    
 
 @dataclass(frozen=True, eq=True, order=True)
-class UserDashboard(ClientView):
+class UserDashboard:
     """
     ClientView instance of a user which doesn't have 
     administrative privileges. It may be associated 
     to both a customer and a business user.
     """
 
-    url : str
-    username : str
+    user_view : UserView
+    shop_view : ShopView
+    fidelity_program_view : FidelityProgramView
+    catalogue_view : CatalogueView
 
     def open_view(self):
         st.title('Project Loyalty Platform')
@@ -85,35 +44,133 @@ class UserDashboard(ClientView):
         st.write('LOGOUT')
 
     def home(self):
-        user_profile, change_password = st.tabs(['User profile', 'Change password'])
-        user = self.get_self_user()
-        if user:
-            with user_profile:
-                user.as_form()
-            with change_password:
-                user.as_password_update_form()      
+        return self.user_view.open_view() 
 
     def sideBar(self): 
         with st.sidebar as sidebar:
             selected=option_menu(
                 menu_title="Main Menu",
-                options=['Home', 'Shops', 'Logout'],
-                icons=['house', 'cart', 'dash-circle'],
+                options=['Home', 'Shops', 'Fidelity Programs', 'Logout'],
+                icons=['house', 'cart', 'people', 'dash-circle'],
                 menu_icon="cast",
                 default_index=0
             )
         if selected=="Home":
-            home_col1, home_col2 = st.columns(2)
-            with home_col1:
-                self.home()
-            with home_col2:
-                st.markdown("Welcome to Project Loyalty Platfom.\nThis application helps customers and companies in tightning each others' bonds and in finding the best opportunities.\n Personal data is treated according to EU policies.\n For further clarification and for additional information, send an email to projectloyalty@gmail.com")
+            self.home()
         if selected=='Shops':
-            all_shops, selected_shop = st.columns(2)
-            with all_shops:
-                shops = self.get_shop_list()
-            with selected_shop:
-                if shops and shops.size != 0:
-                    st.write(self.get_shop(shops.iloc[0].to_dict()['name']).as_form())
+            self.shop_view.open_view()
+        if selected=='Fidelity Programs':
+            self.fidelity_program_view.open_view()
+                    #st.write(fprograms)
+                #with fprog_col2:
+                #    if fprograms.size != 0:
+                        #for prog in fprograms.loc[2:3,'url']:
+                #            st.write(FidelityProgramDetail(fprograms.iloc[0].to_dict()['url']))
         if selected=='Logout':
             self.close_view()
+
+
+@dataclass(frozen=True, eq=True, order=True)
+class UserView:
+    user : UserDetail
+
+    def open_view(self):
+        home_col1, home_col2 = st.columns(2)
+        with home_col1:
+            user_profile, change_password = st.tabs(['User profile', 'Change password'])
+            with user_profile:
+                self.get_self_user().show()
+            with change_password:
+                UserSetPasswordForm(self.user).show()
+        with home_col2:
+            st.markdown("Welcome to Project Loyalty Platfom.\nThis platform aims to provide a common hub in which commercial businesses present themselves to the public and communicate with each other and with the customers themselves.\nCommercial establishments can join together in common loyalty programmes, channels constituting a way through which a customer is able to obtain economic advantages and rewards thanks to the loyalty towards the commercial establishment or the consortium of companies participating in the program itself, while commercial businesses can adopt strategies aimed at retaining existing customers and obtaining new ones.\nFor further clarification and for additional information, send an email to projectloyalty@gmail.com")
+
+    def close_view(self):
+        raise NotImplementedError
+
+    def get_user(self, username: str):
+        return UserForm(UserDetail(url=users_endpoint+username).get())
+    
+    def get_self_user(self):
+        return UserForm(self.user)
+    
+    def user_create_form(self):
+        return UserCreateForm(UserDetail)
+
+@dataclass(frozen=True, eq=True, order=True)
+class ShopView:
+    shop : ShopDetail | None = None
+
+    def open_view(self):
+        all_shops, selected_shop = st.columns(2)
+        with all_shops:
+            shops = self.get_all_shops().show()
+        with selected_shop:
+            if len(shops.selected_rows) > 0:
+                self.get_shop(shops.selected_rows[0]['name']).show()
+
+    def close_view(self):
+        raise NotImplementedError
+
+    def get_shop(self, shopname: str):
+        return ShopForm(ShopDetail(url=shops_endpoint+shopname).get())
+    
+    def get_session_shop(self):
+        return ShopForm(self.shop) if self.shop is not None else st.write('No shop associated to this user')
+    
+    def get_all_shops(self):
+        return Table(element=ShopList(), columns=['url', 'name', 'location'], hidden_columns=['url'])
+
+@dataclass(frozen=True, eq=True, order=True)
+class FidelityProgramView:
+
+    def open_view(self):
+        with st.container() as container:
+            fprog_col1, fprog_col2 = st.columns(spec=2, gap='large')
+            fprograms = None
+            with fprog_col1:
+                if fprograms is None:
+                    fprograms = self.get_all_fidelity_programs().show()
+            with fprog_col2:
+                if len(fprograms.selected_rows) > 0:
+                    st.write(self.get_fidelity_program(fprograms.selected_rows[0]['name']).show())
+
+    def close_view(self):
+        raise NotImplementedError
+
+    def get_all_fidelity_programs(self):
+        return Table(element=FidelityProgramList(), columns=['url', 'name', 'program_type'], hidden_columns=['url'])
+
+    def get_all_fidelity_programs_by_shop(self, shopname: str):
+        return Table(element=FidelityProgramList(api_endpoint=fidelity_programs_endpoint+'byshop/'+shopname), columns=['url', 'name', 'program_type'], hidden_columns=['url'])
+    
+    def create_fidelity_program(self):
+        return FidelityProgramCreateForm(FidelityProgramDetail())
+    
+    def get_fidelity_program(self, programname: str):
+        return FidelityProgramForm(FidelityProgramDetail(url=fidelity_programs_endpoint+programname).get())
+    
+
+@dataclass(frozen=True, eq=True, order=True)
+class CatalogueView:
+
+    def create_catalogue_element(self):
+        return CatalogueCreateForm(CatalogueDetail())
+    
+    def get_all_catalogue_elements(self):
+        return Table(element=CatalogueList(), columns=['url', 'customer', 'fidelity_program'], hidden_columns=['url'])
+
+    def get_all_catalogue_elements_by_user(self, username: str):
+        return Table(element=CatalogueList(api_endpoint=catalogue_endpoint+username), columns=['url', 'customer', 'fidelity_program'], hidden_columns=['url'])
+    
+    def get_all_catalogue_elements_by_fidelity_program(self, programname: str):
+        return Table(element=CatalogueList(api_endpoint=catalogue_endpoint+programname), columns=['url', 'customer', 'fidelity_program'], hidden_columns=['url'])
+    
+    def get_catalogue_element(self, username: str, programname: str):
+        return CatalogueForm(CatalogueDetail(api_endpoint=catalogue_endpoint+username+'/'+programname).get())
+    
+    def delete_catalogue_element(self, username: str, programname: str):
+        return self.get_catalogue_element(username, programname).element.delete()
+
+    def update_catalogue_element(self, username: str, programname: str):
+        return CatalogueUpdateForm(self.get_catalogue_element(username, programname))
