41ca3ef7039f68b78331320c51043369aeec7373
Samuele Stronati <samuele.stronati@studenti.unicam.it>
2023-12-13 10:37:47 +0100

    Added transaction and product model to server. Added login service

--- server/project/server/urls.py
+++ server/project/server/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from .views import UserViewSet, ShopViewSet, FidelityProgramViewSet, CatalogueViewSet
+from .views import UserViewSet, ShopViewSet, FidelityProgramViewSet, CatalogueViewSet, ProductViewSet, \
+    TransactionViewSet, CustomAuthToken
 
 # Create a router and register viewsets with it.
 router = DefaultRouter()
@@ -9,11 +10,13 @@ router.register(r'users', UserViewSet, basename="user")
 router.register(r'shops', ShopViewSet, basename="shop")
 router.register(r'fidelityprograms', FidelityProgramViewSet, basename='fidelityprogram')
 router.register(r'catalogue', CatalogueViewSet, basename='catalogue')
-
+router.register(r'product', ProductViewSet, basename='product')
+router.register(r'transactions', TransactionViewSet, basename='transaction')
 
 # The API URLs are determined automatically by the router.
 urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api-token-auth/', CustomAuthToken.as_view()),
     path(r'', include(router.urls)),
 ]
